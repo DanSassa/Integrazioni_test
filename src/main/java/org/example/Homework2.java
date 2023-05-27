bf6a4a77a052ea467005c467f738c1982cc35322
DanSassa <d.sassarola@studenti.uniba.it>
2023-05-27 19:22:04 +0200

    modifica codice 4

--- src/main/java/org/example/Homework2.java
+++ src/main/java/org/example/Homework2.java
@@ -5,24 +5,23 @@ public class Homework2{
         public int OperationsBetweenNumbers(int var1, int var2) {
 
             int op=0;
-            if(var1 ==0 || var2 == 0)
+            if(var1 ==0 && var2 == 0)
                 throw new IllegalArgumentException();
-            else{
-                if (var1 >0 && var2 >0){
+           else{
+                if (var1 >0 && var2 >=0){
                     op= var1+var2;
                     System.out.println("somma: ("+ var1 + ")+(" + var2 + ") =" + op);
                 } else if (var1 <0 && var2 <0) {
                     op= var1-var2;
                     System.out.println("differenza: ("+ var1 + ")-(" + var2 + ") =" + op);
-                } else if (var1>=0 && var2 <0) {
+                } else if (var1>=0 && var2 <0 ) {
                     op= var1*var2;
                     System.out.println("moltiplicazione: ("+ var1 + ")*(" + var2 + ") =" + op);
-                } else{
+                } else if((var1 <0 && var2>0) || (var1 ==0 && var2 >0)){
                     op=var1/var2;
                     System.out.println("divisione: ("+ var1 + ")/(" + var2 + ") =" + op);
                 }
             }
-
             return op;
         }
 
--- src/test/java/org/example/Homework2Test.java
+++ src/test/java/org/example/Homework2Test.java
@@ -6,20 +6,48 @@ import org.junit.jupiter.api.Test;
 
 public class Homework2Test {
     Homework2 homework2 = new Homework2();
+    @Test
+    @DisplayName("test sulla correttezza dei risultati")
+    void testRisultatoCorretto() {
+        Assertions.assertEquals(4, homework2.OperationsBetweenNumbers(2,2));
+        Assertions.assertEquals(40, homework2.OperationsBetweenNumbers(25,15));
+        Assertions.assertEquals(-20, homework2.OperationsBetweenNumbers(10,-2));
+    }
+
 
     @Test
-    void testOperazione() {
+    void testZeri() {
+        //T1
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> homework2.OperationsBetweenNumbers(55,0));
+        //T2
         Assertions.assertThrows(IllegalArgumentException.class, ()-> homework2.OperationsBetweenNumbers(0,1));
-        Assertions.assertThrows(IllegalArgumentException.class, ()-> homework2.OperationsBetweenNumbers(1,0));
-        Assertions.assertThrows(IllegalArgumentException.class, ()-> homework2.OperationsBetweenNumbers(0,0));
     }
 
     @Test
-    @DisplayName("test sul risultato della somma")
-    void testRisultatoCorretto() {
-        Assertions.assertEquals(4, homework2.OperationsBetweenNumbers(2,2));
-        Assertions.assertEquals(0, homework2.OperationsBetweenNumbers(-2,-2));
-        Assertions.assertEquals(-8, homework2.OperationsBetweenNumbers(4,-2));
-        Assertions.assertEquals(-1, homework2.OperationsBetweenNumbers(-2,2));
+    @DisplayName("test sulla somma dei due valori")
+    void testSomma() {
+        //T3
+        Assertions.assertEquals(10, homework2.OperationsBetweenNumbers(5,5));
+    }
+
+    @Test
+    @DisplayName("test sulla sottrazione dei due valori")
+    void testSottrazione() {
+        //T4
+        Assertions.assertEquals(-4, homework2.OperationsBetweenNumbers(-8,-4));
+    }
+
+    @Test
+    @DisplayName("test sulla moltiplicazione dei due valori")
+    void testMoltiplicazione() {
+        //T5
+        Assertions.assertEquals(-49, homework2.OperationsBetweenNumbers(7,-7));
+    }
+
+    @Test
+    @DisplayName("test sulla moltiplicazione dei due valori")
+    void testDivisione() {
+        //T5
+        Assertions.assertEquals(-5, homework2.OperationsBetweenNumbers(-10,2));
     }
 }
